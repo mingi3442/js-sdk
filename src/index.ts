b21495ce29bed464849161069422767974eac544
Lee MinGi <mingi3442@gmail.com>
2024-03-15 16:49:07 +0900

    Module Test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
-class A {
+import Wallet from "ethereumjs-wallet";
+
+export class A {
   constructor() {
     console.log("A");
   }
   con(): void {
     console.log("con");
+  }
+  create(): void {
+    console.log("create");
+    const wallet = Wallet.generate();
+    console.log("pk : ", wallet.getPrivateKeyString());
+    console.log("Address : ", wallet.getPublicKeyString());
   }
 }
