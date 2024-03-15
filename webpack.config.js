837ce5b63b777fe11dae938cd38c6966bbd813f1
Lee MinGi <mingi3442@gmail.com>
2024-03-15 17:29:34 +0900

    Webpack test

--- webpack.config.js
+++ webpack.config.js
@@ -22,11 +22,13 @@ module.exports = {
       assert: require.resolve("assert/"),
       vm: require.resolve("vm-browserify"),
       buffer: require.resolve("buffer/"),
+      process: require.resolve("process/browser"),
     },
   },
   plugins: [
     new webpack.ProvidePlugin({
       Buffer: ["buffer", "Buffer"],
+      process: "process/browser",
     }),
   ],
   output: {
